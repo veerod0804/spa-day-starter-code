2a99995183f39626dc6af66a4c254bafeabfd9be
Victoria <veerod0804@yahoo.com>
2022-10-10 20:45:09 -0500

    Completed studio model validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,35 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+    @NotNull
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+
+    @NotNull
+    @Email(message = "Please provide a valid email address.")
     private String email;
+
+    @NotNull
+    @NotBlank
+    @Min(6)
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +54,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword(password);
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword(password);
+    }
+
+    private void checkPassword(String password) {
+        if (this.password != null && this.verifyPassword != null && !this.password.equals(this.verifyPassword)) {
+            this.verifyPassword = null;
+        }
     }
 }
